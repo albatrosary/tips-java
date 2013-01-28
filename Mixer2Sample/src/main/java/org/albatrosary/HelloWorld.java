a57ac754b02007738010d526323d387592507176
Fumio Sagawa <fumio.sagawa@gmail.com>
2013-01-28 10:25:33 +0900

    change of HTML file path

--- Mixer2Sample/src/main/java/org/albatrosary/HelloWorld.java
+++ Mixer2Sample/src/main/java/org/albatrosary/HelloWorld.java
@@ -14,7 +14,9 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) throws Exception {
         Mixer2Engine m2e = new Mixer2Engine();
-        Html html = m2e.loadHtmlTemplate(new File("D:\\albatorosary-workspace\\Mixer2\\bin\\HelloWorld.html"));
+        File file = null;
+        Html html = m2e.loadHtmlTemplate(file);
+        
         html.getById("hellomsg",Div.class).getContent().clear();
         html.getById("hellomsg",Div.class).getContent().add("Hello World !");
         System.out.println(m2e.saveToString(html));
